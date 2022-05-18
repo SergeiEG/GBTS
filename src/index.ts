22040b9b4d471790a1b8ef6076b7572a503a0f0a
Sergei <1og@mail.ru>
2022-05-18 21:55:19 +0600

    zaglywka

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prefer-const */
+import { renderSearchFormBlock } from './search-form.js';
+import { renderSearchStubBlock } from './search-results.js';
+import { renderUserBlock } from './user.js';
+import { renderToast } from './lib.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+  let entryDef = new Date;
+  entryDef.setDate(entryDef.getDate() + 1);
+  let depDef = new Date;
+  depDef.setDate(depDef.getDate() + 3);
+
+  renderUserBlock(2, 'Wade KApss', '/img/avatar.png');
+  renderSearchFormBlock(entryDef, depDef);
+  renderSearchStubBlock();
+  renderToast(
+    {
+      text: 'Это пример уведомления. Используйте его при необходимости',
+      type: 'success',
+    },
+    {
+      name: 'Понял',
+      handler: () => {
+        console.log('Уведомление закрыто');
+      },
+    }
+  );
+});
